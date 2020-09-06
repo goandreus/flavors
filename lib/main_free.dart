1c56fcdb539e90e5d17da0d00fd8a05fc44eac80
goandreus <andres.chavez.crisostomo@gmail.com>
2020-09-06 15:56:18 -0500

    free apihost git push

---
+++ lib/main_free.dart
@@ -0,0 +1,16 @@
+import 'package:flavors/app_config.dart';
+import 'package:flutter/material.dart';
+
+import 'my_app.dart';
+
+void main() {
+  AppConfig.instance.init(
+    apiHost: 'https:free.apihost.com', 
+    icon: 'assets/icon/free.png', 
+    primaryColor: Colors.blue, 
+    secondaryColor: Colors.white);
+
+    runApp(MyApp());
+}
+
+
