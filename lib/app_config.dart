6355ca8eac2e62d282e58bf045e3c09ff189281f
goandreus <andres.chavez.crisostomo@gmail.com>
2020-09-29 15:17:14 -0500

    instance

--- lib/app_config.dart
+++ lib/app_config.dart
@@ -1,6 +1,29 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:meta/meta.dart';
+
 class AppConfig{
+
+  String _apiHost, _icon;
+  Color _primaryColor;
+  Color _secondaryColor;
+
   AppConfig._internal();
   static final AppConfig _instance = AppConfig._internal();
-
   static AppConfig get instance => _instance;
+
+  void init ({
+    @required String apiHost,
+    @required String icon,
+    @required Color primaryColor,
+    @required Color secondaryColor
+  }) {
+    this._apiHost = apiHost;
+    this._icon = icon;
+    this._primaryColor = primaryColor;
+    this._secondaryColor = secondaryColor;
+  }
+
+
 }
